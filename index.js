deef0ddcb461cb50f864a5fe9ccc984ce33c0d0c
adnan2508 <adnandiu2800@gmail.com>
2024-02-06 23:55:37 +0600

    updated

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-const employees = [
-    { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
-    { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
-    { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
-    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
-];
+const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed', 'n'];
 
-function findAveragePhonePrice (employees){
-    let cost = 0;
-    let currentSalary;
-    let salary;
-    let totalSalary = 0;
-    
-    for(const employee of employees) {
-        currentSalary = employee.experience * employee.increment;
-        salary = currentSalary + employee.starting;
-        totalSalary = totalSalary + salary;
+function smallest (heights2) {
+    let small = heights2[0].length;
+    let smallFriend;
+    for (const height of heights2){
+        if(height.length < small) {
+            smallFriend = height;
+        }
     }
-    return totalSalary;
+    return smallFriend;
 }
 
-const number = findAveragePhonePrice(employees);
-console.log(number);
+console.log(smallest(heights2));
