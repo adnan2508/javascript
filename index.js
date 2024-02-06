7bcc15435b6f85166483eb3511433dad6b8661d9
adnan2508 <adnandiu2800@gmail.com>
2024-02-06 20:36:13 +0600

    added

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function nameThis (parameter) {
-        console.log(`HI there!`);
+const heights2 = [167, 190, 120, 165, 137];
+
+function lowest (heights2){
+        for (const height of heights2){
+            console.log(height);
+        }
 }
 
-nameThis();
+const number = lowest(heights2);
+console.log(number);
